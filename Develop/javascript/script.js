93c9fde834066daedb2692ccc58df4fefe685c92
Arvin John Bautista <arvinyoung01@gmail.com>
2021-04-01 14:23:56 -0700

    Syn and ant added

--- Develop/javascript/script.js
+++ Develop/javascript/script.js
@@ -1,4 +1,4 @@
-var apiKey = "563492ad6f9170000100000177a465d3b96f4c42ada52578bcec403f"; //api key from Nick
+var apiKey = "563492ad6f9170000100000177a465d3b96f4c42ada52578bcec403f"; // API key from Nick
 var pictureContainer = document.getElementById("picture-container");
 
 function imageSearch(searchTerm) {
@@ -9,7 +9,7 @@ function imageSearch(searchTerm) {
         },
         url: "https://api.pexels.com/v1/search?query=" + searchTerm + "&per_page=15&page=1",
         success: function (data) {
-            console.log(data);
+            // console.log(data);
             pictureContainer.innerHTML = "";
             printImage(data);
             printImageSmall(data);
@@ -21,24 +21,24 @@ function imageSearch(searchTerm) {
 }
 
 function printImage(data) {
-    
+
     var firstRow = document.createElement("div");
-    firstRow.setAttribute("class","row");
-    firstRow.setAttribute("style","max-height:800px;display:flex;justify-content:center;");
+    firstRow.setAttribute("class", "row");
+    firstRow.setAttribute("style", "max-height:800px;display:flex;justify-content:center;");
 
     var firstRowDiv = document.createElement("div");
-    firstRowDiv.setAttribute("style","position:relative;max-height:100%;display:flex;justify-content:center;");
-    firstRowDiv.setAttribute("class","padded");
+    firstRowDiv.setAttribute("style", "position:relative;max-height:100%;display:flex;justify-content:center;");
+    firstRowDiv.setAttribute("class", "padded");
 
     var firstPic = document.createElement("img");
-    firstPic.setAttribute("src",data.photos[0].src.original);
-    firstPic.setAttribute("style","max-height:100%;border-radius:50px;");
-    firstPic.setAttribute("class","sample-image");
+    firstPic.setAttribute("src", data.photos[0].src.original);
+    firstPic.setAttribute("style", "max-height:100%;border-radius:50px;");
+    firstPic.setAttribute("class", "sample-image");
 
     var link = document.createElement("a");
-    link.setAttribute("href",data.photos[0].url);
-    link.setAttribute("target","_blank");
-    link.setAttribute("class","absolute bottom half-padded authorLink double-gap-bottom small");
+    link.setAttribute("href", data.photos[0].url);
+    link.setAttribute("target", "_blank");
+    link.setAttribute("class", "absolute bottom half-padded authorLink double-gap-bottom small");
     link.textContent = "Picture taken by: " + data.photos[0].photographer;
 
     firstRowDiv.appendChild(firstPic);
@@ -50,24 +50,24 @@ function printImage(data) {
 function printImageSmall(data) {
 
     var secondRow = document.createElement("div");
-    secondRow.setAttribute("class","row");
-    secondRow.setAttribute("style","max-height:500px;display:flex;justify-content:center;");
+    secondRow.setAttribute("class", "row");
+    secondRow.setAttribute("style", "max-height:500px;display:flex;justify-content:center;");
 
-    for (var i=1;i<4;i++) {
+    for (var i = 1; i < 4; i++) {
 
         var secondRowDiv = document.createElement("div");
-        secondRowDiv.setAttribute("style","position:relative;max-height:100%;display:flex;justify-content:center;");
-        secondRowDiv.setAttribute("class","padded one third");
+        secondRowDiv.setAttribute("style", "position:relative;max-height:100%;display:flex;justify-content:center;");
+        secondRowDiv.setAttribute("class", "padded one third");
 
         var secondPic = document.createElement("img");
-        secondPic.setAttribute("src",data.photos[i].src.original);
-        secondPic.setAttribute("style","border-radius:25px;");
-        secondPic.setAttribute("class","sample-image");
+        secondPic.setAttribute("src", data.photos[i].src.original);
+        secondPic.setAttribute("style", "border-radius:25px;");
+        secondPic.setAttribute("class", "sample-image");
 
         var link = document.createElement("a");
-        link.setAttribute("href",data.photos[i].url);
-        link.setAttribute("target","_blank");
-        link.setAttribute("class","absolute bottom half-padded authorLink double-gap-bottom small");
+        link.setAttribute("href", data.photos[i].url);
+        link.setAttribute("target", "_blank");
+        link.setAttribute("class", "absolute bottom half-padded authorLink double-gap-bottom small");
         link.textContent = "Picture taken by: " + data.photos[i].photographer;
 
         secondRowDiv.appendChild(secondPic);
@@ -86,6 +86,7 @@ var wordSearchResultsBody = document.getElementById("wordBody");
 form.addEventListener("submit", function (event) {
     event.preventDefault();
     wordFromMerriamCollegiate(searchWord.value);
+    SynAndAntoFromMerriamCollegiate(searchWord.value);
     imageSearch(searchWord.value);
 })
 function wordFromMerriamCollegiate(userGivenWord) {
@@ -114,7 +115,7 @@ function wordFromMerriamCollegiate(userGivenWord) {
 function getAndDisplayWord(searchResults) {
     wordSearchResultsHeader.innerHTML = "";
     firstResult = searchResults[0]; // Assigned the array data from response to firstResult
-    console.log(firstResult);
+    // console.log(firstResult);
     wordSearched = document.createElement("h2");
     wordSearched.textContent = searchWord.value;
     wordSearched.classList.add("padded", "museo-slab");
@@ -170,6 +171,60 @@ function getAndDisplayWord(searchResults) {
     wordSearchResultsBody.appendChild(wordDefinitionTitle);
     hrAfterDefinition = document.createElement("hr");
     wordDefinitions.appendChild(hrAfterDefinition);
+
+}
+
+function SynAndAntoFromMerriamCollegiate(userGivenWord) {
+    var merriamthesaurusAPI = "https://dictionaryapi.com/api/v3/references/thesaurus/json/" + userGivenWord
+        + "?key=b99aa4f1-0306-436d-ae05-cb4ddae328a0"; // API from Nick
+    fetch(merriamthesaurusAPI)
+        .then(function (response2) {
+            // console.log(response1.status);
+            if (response2.ok) {
+                response2.json().then(function (data) {
+                    console.log(data);
+                    if (data[0].meta) { // Checking if the word is a legit
+                        getAndDisplaySynAndAnt(data);
+                    } else {
+                        alert("Sorry, No Synonyms/Antonyms can be found");
+                    }
+                });
+            } else {
+                alert("Cannot found");
+            }
+        })
+}
+
+function getAndDisplaySynAndAnt(data) {
+    wordSynTitle = document.createElement("h3");
+    wordSynTitle.classList.add("padded", "quicksand");
+    wordSynTitle.innerHTML = "Synonym(s): <br>";
+    wordSearchResultsBody.appendChild(wordSynTitle);
+    wordSyn = document.createElement("p");
+    wordSyn.classList.add("padded", "quicksand", "wordDefinitions");
+    if (data[0].meta.syns.length == 0) {
+        wordSyn.textContent = "Not available";
+    } else {
+        for (var i = 0; i < data[0].meta.syns[0].length; i++) {
+            wordSyn.textContent += data[0].meta.syns[0][i] + ", ";
+        }
+    }
+    wordSynTitle.appendChild(wordSyn);
+
+    wordAntTitle = document.createElement("h3");
+    wordAntTitle.classList.add("padded", "quicksand");
+    wordAntTitle.innerHTML = "Antonym(s): <br>";
+    wordSearchResultsBody.appendChild(wordAntTitle);
+    wordAnt = document.createElement("p");
+    wordAnt.classList.add("padded", "quicksand", "wordDefinitions");
+    if (data[0].meta.ants.length == 0) {
+        wordAnt.textContent = "Not available";
+    } else {
+        for (var i = 0; i < data[0].meta.ants[0].length; i++) {
+            wordAnt.textContent += data[0].meta.ants[0][i] + ", ";
+        }
+    }
+    wordAntTitle.appendChild(wordAnt);
 
 }
 
